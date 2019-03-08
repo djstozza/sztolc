6e3baf19f06cb2d8cba0818457f10072e79da02f
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2019-03-09 10:10:46 +1100

    Test recaptcha refactoring

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,43 @@ let recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPT
 
 let router = express.Router();
 
+var submitMessage = function (req, res) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.json({ errors: errors.array() });
+  }
+
+  let mailOpts, smtpTrans;
+
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USERNAME,
+      pass: process.env.GMAIL_PASSWORD
+    }
+  });
+
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: process.env.GMAIL_USERNAME,
+    subject: req.body.subject,
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+  };
+
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.json({ error: error });
+    } else {
+      res.json({
+        success: true, message: "Thanks for contacting me! I look forward to getting in touch with you shortly."
+      });
+    }
+  });
+}
+
 
 /* GET home page. */
 router.get('/', recaptcha.middleware.render, function(req, res, next) {
@@ -24,45 +61,14 @@ router.post(
   ],
   function (req, res) {
     recaptcha.verify(req, function(error, data) {
-      console.log(req.recaptcha)
-      if (req.recaptcha.error) {
-        return console.log('foo')
-      }
-    });
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.json({ errors: errors.array() });
-    }
-
-    let mailOpts, smtpTrans;
-
-    smtpTrans = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.GMAIL_USERNAME,
-        pass: process.env.GMAIL_PASSWORD
-      }
-    });
-
-    mailOpts = {
-      from: req.body.name + ' &lt;' + req.body.email + '&gt;',
-      to: process.env.GMAIL_USERNAME,
-      subject: req.body.subject,
-      text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
-    };
-
-    smtpTrans.sendMail(mailOpts, function (error, response) {
-      if (error) {
-        res.json({ error: error });
+      if (!req.recaptcha.error) {
+        submitMessage(req, res);
       } else {
-        res.json({
-          success: true, message: "Thanks for contacting me! I look forward to getting in touch with you shortly."
-        });
+        console.log(req.recaptcha.error)
+        res.json({ error: req.recaptcha.error })
       }
     });
-});
+  }
+);
 
 module.exports = router;
