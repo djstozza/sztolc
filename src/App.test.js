f1fa5d43064cde072e6af66e0628eaaff8ca7135
Daniel Sztolcman <djstozza@192-168-1-101.tpgi.com.au>
2024-01-21 22:03:12 +1100

    Update package.json

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
-import ReactDOM from 'react-dom'
+import { act } from 'react-dom/test-utils'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+    act(async () => {
+        const div = document.createElement('div')
+        render(<App />, div)
+    })
 });
